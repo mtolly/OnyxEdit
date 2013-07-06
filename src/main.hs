eef5046b133f928fa8f5e61a0389f45e0f5e07d5
Michael Tolly <miketolly@gmail.com>
2013-07-06 15:02:43 -0500

    massive changes to work with new Position format

--- src/main.hs
+++ src/main.hs
@@ -98,50 +98,69 @@ instance Ord Position where
 instance Eq Position where
   x == y = compare x y == EQ
 
-data Program = Program
-  { vSurfaces   :: Surfaces
-  , vSources    :: Sources
-  , vDrumChart  :: Map.Map Seconds (Set.Set Note)
-  , vPosition   :: Seconds
-  , vResolution :: Int
-  -- ^ Zoom level, in pixels (width) per second of time
-  , vPlaying    :: Bool
-  -- ^ Is audio currently playing?
-  , vPlaySpeed  :: Rational
-  , vTempos     :: Map.Map Seconds (Beats, BPS)
-  -- ^ Tempo events to convert from seconds to beats.
-  , vTemposRev  :: Map.Map Beats (Seconds, BPS)
-  -- ^ Tempo events to convert from beats to seconds.
-  , vMeasures   :: Map.Map Beats (Int, Beats)
-  , vDivision   :: Beats
-  -- ^ The fraction of a beat that creates sub-beat lines.
-  , vLines      :: Map.Map Seconds Line
-  , vMetronome  :: Bool
-  } deriving (Eq, Ord, Show)
+toSeconds :: Position -> Seconds
+toSeconds (Both s _) = s
+toSeconds (Seconds s) = s
+toSeconds (Beats _) = error "toSeconds: got Beats value"
 
-secondsToBeats' :: Map.Map Seconds (Beats, BPS) -> Seconds -> Maybe Beats
-secondsToBeats' tmps secs = flip fmap (Map.lookupLE secs tmps) $
-  \(secs', (bts, bps)) -> bts + (secs - secs') * bps
+toBeats :: Position -> Seconds
+toBeats (Both _ b) = b
+toBeats (Beats b) = b
+toBeats (Seconds _) = error "toBeats: got Seconds value"
+
+positionTempos :: Map.Map Beats BPS -> Map.Map Position BPS
+positionTempos = Map.fromDistinctAscList . f 0 0 2 . Map.toAscList where
+  f :: Beats -> Seconds -> BPS -> [(Beats, BPS)] -> [(Position, BPS)]
+  f bts secs bps xs = case xs of
+    [] -> []
+    (bts', bps') : xs' -> let
+      secs' = secs + (bts' - bts) / bps
+      in (Both secs' bts', bts') : f bts' secs' bps' xs'
+
+positionTrack :: Map.Map Position BPS -> Map.Map Beats a -> Map.Map Position a
+positionTrack tmps = Map.mapKeysMonotonic $
+  \bts -> Both (beatsToSeconds' tmps bts) bts
+
+secondsToBeats' :: Map.Map Position BPS -> Seconds -> Beats
+secondsToBeats' tmps secs = case Map.lookupLE (Seconds secs) tmps of
+  Nothing -> error "secondsToBeats: missing tempo"
+  Just (Both secs' bts, bps) -> bts + (secs - secs') * bps
+  Just _ -> error "secondsToBeats: invalidly stored tempo"
 
 secondsToBeats :: Seconds -> Prog Beats
 secondsToBeats secs = do
-  tmps <- gets vTempos
-  case secondsToBeats' tmps secs of
-    Nothing -> error $
-      "secondsToBeats: no tempo event before " ++ show secs ++ " seconds"
-    Just bts -> return bts
+  tmps <- gets $ vTempos . vTracks
+  return $ secondsToBeats' tmps secs
 
-beatsToSeconds' :: Map.Map Beats (Seconds, BPS) -> Beats -> Maybe Seconds
-beatsToSeconds' tmps bts = flip fmap (Map.lookupLE bts tmps) $
-  \(bts', (secs, bps)) -> secs + (bts - bts') / bps
+beatsToSeconds' :: Map.Map Position BPS -> Beats -> Seconds
+beatsToSeconds' tmps bts = case Map.lookupLE (Beats bts) tmps of
+  Nothing -> error "beatsToSeconds: missing tempo"
+  Just (Both secs bts', bps) -> secs + (bts - bts') / bps
+  Just _ -> error "beatsToSeconds: invalidly stored tempo"
 
 beatsToSeconds :: Beats -> Prog Seconds
 beatsToSeconds bts = do
-  tmps <- gets vTemposRev
-  case beatsToSeconds' tmps bts of
-    Nothing -> error $
-      "beatsToSeconds: no tempo event before " ++ show bts ++ " beats"
-    Just secs -> return secs
+  tmps <- gets $ vTempos . vTracks
+  return $ beatsToSeconds' tmps bts
+
+data Tracks = Tracks
+  { vTempos   :: Map.Map Position BPS
+  , vDrums    :: Map.Map Position (Set.Set Note)
+  , vMeasures :: Map.Map Position (Int, Beats)
+  , vLines    :: Map.Map Position Line
+  } deriving (Eq, Ord, Show, Read)
+
+data Program = Program
+  { vSurfaces   :: Surfaces
+  , vSources    :: Sources
+  , vTracks     :: Tracks
+  , vPosition   :: Position
+  , vResolution :: Int -- ^ Zoom level, in pixels (width) per second of time
+  , vPlaying    :: Bool -- ^ Is audio currently playing?
+  , vPlaySpeed  :: Rational
+  , vDivision   :: Beats -- ^ The beat fraction that creates sub-beat lines.
+  , vMetronome  :: Bool
+  } deriving (Eq, Ord, Show)
 
 makeMeasure :: Beats -> Beats -> (Int, Beats) -> Map.Map Beats Line
 makeMeasure dvn start (mult, unit) = let
@@ -155,12 +174,15 @@ makeMeasure dvn start (mult, unit) = let
   in measure `Map.union` beats `Map.union` subbeats
 
 makeLines :: Prog ()
+makeLines = return ()
+{-
 makeLines = do
   msrs <- fmap Map.toAscList $ gets vMeasures
   dvn <- gets vDivision
   let btLns = concatMap (Map.toAscList . uncurry (makeMeasure dvn)) msrs
   secLns <- mapM (runKleisli $ first $ Kleisli beatsToSeconds) btLns
   modify $ \prog -> prog { vLines = Map.fromDistinctAscList secLns }
+-}
 
 data Surfaces = Surfaces
   { vScreen     :: Surface
@@ -193,15 +215,16 @@ apply x y src dst = blitSurface src Nothing dst (Just offset)
 playUpdate :: Prog ()
 playUpdate = gets vPlaying >>= \b -> when b $ do
   pos <- gets vPosition
-  lns <- gets vLines
+  lns <- gets $ vLines . vTracks
   (dl, _) <- gets $ vDrumAudio . vSources
   t <- liftIO $ Sound.ALUT.get $ secOffset dl
   a <- gets $ vAudioStart . vSources
   let pos' = realToFrac $ t - a
-  modify $ \prog -> prog { vPosition = pos' }
+  bts <- secondsToBeats pos' 
+  modify $ \prog -> prog { vPosition = Both pos' bts }
   met <- gets vMetronome
-  when (pos' > pos && met) $ case Map.splitLookup pos lns of
-    (_, eq, gt) -> case Map.splitLookup pos' gt of
+  when (pos' > toSeconds pos && met) $ case Map.splitLookup pos lns of
+    (_, eq, gt) -> case Map.splitLookup (Seconds pos') gt of
       (lt, _, _) -> let
         startBeat = elem eq [Just Measure, Just Beat]
         passedBeat = any (/= SubBeat) $ Map.elems lt
@@ -215,11 +238,11 @@ timeToX :: Seconds -> Prog Int
 timeToX pos = do
   now <- gets vPosition
   pps <- gets vResolution
-  return $ 150 + floor ((pos - now) * fromIntegral pps)
+  return $ 150 + floor ((pos - toSeconds now) * fromIntegral pps)
 
-drawLine :: Seconds -> Line -> Prog ()
+drawLine :: Position -> Line -> Prog ()
 drawLine pos l = void $ do
-  x <- timeToX pos
+  x <- timeToX $ toSeconds pos
   scrn <- gets $ vScreen . vSurfaces
   surf <- gets $ vBeatLines . vSurfaces
   let clip = Just $ case l of
@@ -229,19 +252,20 @@ drawLine pos l = void $ do
       drawAt = Just $ Rect (x - 15) 100 0 0
   liftIO $ blitSurface surf clip scrn drawAt
 
-drawNote :: Seconds -> Note -> Prog ()
+drawNote :: Position -> Note -> Prog ()
 drawNote pos note = void $ do
   surf <- gets $ vNoteSheet . vSurfaces
   scrn <- gets $ vScreen . vSurfaces
-  x <- timeToX pos
+  x <- timeToX $ toSeconds pos
   let (clipX, clipY) = noteSprite note
       clip = Just $ Rect clipX clipY 30 125
       drawAt = Just $ Rect (x - 15) 100 0 0
   liftIO $ blitSurface surf clip scrn drawAt
 
+-- TODO: Limit drawing to notes that are actually visible
 drawNotes :: Prog ()
 drawNotes = do
-  notes <- gets vDrumChart
+  notes <- gets $ vDrums . vTracks
   now <- gets vPosition
   case Map.splitLookup now notes of
     (lt, eq, gt) -> do
@@ -249,12 +273,12 @@ drawNotes = do
       maybe (return ()) (mapM_ (drawNote now) . Set.toList) eq
       drawMore gt
   where
-    drawLess mp = mapM_ (\(pos, nts) -> mapM_ (drawNote pos) $ Set.toList nts) $
-      Map.toList mp
+    drawLess mp = forM_ (Map.toList mp) $ \(pos, nts) ->
+      forM_ (Set.toList nts) $ drawNote pos
     drawMore = drawLess
 
-kitchen :: Map.Map Seconds (Set.Set Note)
-kitchen = Map.fromList $ map (\(sec, st) -> (sec / 2, st))
+kitchen :: Map.Map Beats (Set.Set Note)
+kitchen = Map.fromList
   [ (0  , Set.fromList [Kick, CrashG])
   , (0.5, Set.fromList [HihatF])
   , (1  , Set.fromList [RideB])
@@ -282,6 +306,9 @@ kitchen = Map.fromList $ map (\(sec, st) -> (sec / 2, st))
   , (12  , Set.fromList [Kick, CrashG])
   ]
 
+kitchenMeasures :: Map.Map Beats (Int, Beats)
+kitchenMeasures = Map.fromList $ map (, (3, 1)) [0, 3, 6, 9, 12]
+
 drawBG :: Prog ()
 drawBG = void $ do
   scrn <- gets $ vScreen . vSurfaces
@@ -289,7 +316,7 @@ drawBG = void $ do
   liftIO $ apply 0 0 bg scrn
 
 drawLines :: Prog ()
-drawLines = gets vLines >>= mapM_ (uncurry drawLine) . Map.toList
+drawLines = gets (vLines . vTracks) >>= mapM_ (uncurry drawLine) . Map.toList
 
 drawStaff :: Prog ()
 drawStaff = do
@@ -343,23 +370,25 @@ main = withInit [InitTimer, InitVideo] $ withProgNameAndArgs runALUT $ \_ args -
         , vSongAudio = (srcSongL, srcSongR)
         , vClick = srcClick
         }
+      tmps = Map.singleton (Both 0 0) 2
+      tracks = Tracks
+        { vTempos = tmps
+        , vDrums = positionTrack tmps kitchen
+        , vMeasures = positionTrack tmps kitchenMeasures
+        , vLines = Map.empty -- generated with makeLines
+        }
       prog = Program
         { vSurfaces = surfaces
         , vSources = sources
-        , vDrumChart = kitchen
-        , vPosition = 0
+        , vTracks = tracks
+        , vPosition = Both 0 0
         , vResolution = 200
         , vPlaying = False
         , vPlaySpeed = 1
-        , vTempos = Map.fromList [(0, (0, 2))]
-        , vTemposRev = Map.fromList [(0, (0, 2))]
-        , vMeasures = Map.fromList
-          [(0, (3, 1)), (3, (3, 1)), (6, (3, 1)), (9, (3, 1)), (12, (3, 1))]
         , vDivision = 1/4
-        , vLines = undefined -- generated with makeLines
         , vMetronome = False
         }
-  evalStateT (setPosition 0 >> makeLines >> draw >> inputLoop) prog
+  evalStateT (setPosition (Both 0 0) >> makeLines >> draw >> inputLoop) prog
 
 pauseAndEdit :: ([Source] -> Prog ()) -> Prog ()
 pauseAndEdit f = do
@@ -377,14 +406,24 @@ setSpeed spd = pauseAndEdit $ \srcs -> do
   liftIO $ forM_ srcs $ \src ->
     pitch src $= realToFrac spd
 
-setPosition :: Seconds -> Prog ()
+setPosition :: Position -> Prog ()
 setPosition pos = do
   strt <- gets $ vAudioStart . vSources
-  let pos' = strt + realToFrac pos
+  let pos' = strt + realToFrac (toSeconds pos)
   pauseAndEdit $ \srcs -> do
     modify $ \prog -> prog { vPosition = pos }
     liftIO $ forM_ srcs $ \src ->
       secOffset src $= pos'
+
+setSeconds :: Seconds -> Prog ()
+setSeconds secs = do
+  bts <- secondsToBeats secs
+  setPosition $ Both secs bts
+
+setBeats :: Seconds -> Prog ()
+setBeats bts = do
+  secs <- beatsToSeconds bts
+  setPosition $ Both secs bts
 
 inputLoop :: Prog ()
 inputLoop = do
@@ -407,7 +446,7 @@ inputLoop = do
       setSpeed $ min 2 $ spd + 0.1
     MouseButtonDown _ _ ButtonWheelDown -> do
       pos <- gets vPosition
-      lns <- gets vLines
+      lns <- gets $ vLines . vTracks
       case Map.splitLookup pos lns of
         (_, _, gt) -> if b
           then case reverse $ take 2 $ Map.toAscList gt of
@@ -418,7 +457,7 @@ inputLoop = do
             Nothing          -> return ()
     MouseButtonDown _ _ ButtonWheelUp -> do
       pos <- gets vPosition
-      lns <- gets vLines
+      lns <- gets $ vLines . vTracks
       case Map.splitLookup pos lns of
         (lt, _, _) -> if b
           then case reverse $ take 3 $ Map.toDescList lt of
@@ -445,7 +484,7 @@ inputLoop = do
       g <- liftIO $ Sound.ALUT.get $ sourceGain srcSongL
       forM_ [srcSongL, srcSongR] $ \src ->
         liftIO $ sourceGain src $= if g > 0.5 then 0 else 1
-    KeyDown (Keysym SDLK_z _ _) -> setPosition 0
+    KeyDown (Keysym SDLK_z _ _) -> setPosition (Both 0 0)
     KeyDown (Keysym SDLK_m _ _) ->
       modify $ \prog -> prog { vMetronome = not $ vMetronome prog }
     KeyDown (Keysym SDLK_q _ _) -> do
@@ -466,6 +505,8 @@ inputLoop = do
   inputLoop
 
 toggleDrum :: Note -> Prog ()
+toggleDrum _ = return ()
+{-
 toggleDrum n = do
   now <- gets vPosition
   modify $ \prog -> prog { vDrumChart = Map.alter f now $ vDrumChart prog }
@@ -475,10 +516,9 @@ toggleDrum n = do
             then Nothing
             else Just $ Set.delete n notes
           else Just $ Set.insert n notes
+-}
 
-loadTempos :: Map.Map Beats BPS -> Prog ()
-loadTempos = undefined
-
+{-
 loadMIDI :: File.T -> Prog ()
 loadMIDI f = case MIDI.readFile f of
   Nothing -> error "Invalid MIDI file"
@@ -487,5 +527,5 @@ loadMIDI f = case MIDI.readFile f of
     rtbTempos = Status.toRTB' $ MIDI.tempoTrack fBeats
     mapTempos = fmap realToFrac $ Map.mapKeysMonotonic realToFrac $
       Map.fromAscList $ ATB.toPairList $ RTB.toAbsoluteEventList 0 rtbTempos
-    in do
-      loadTempos mapTempos
+    in undefined
+-}
